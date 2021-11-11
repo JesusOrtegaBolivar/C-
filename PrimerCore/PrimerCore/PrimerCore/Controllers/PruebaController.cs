fcd642e9480cd161caa322445dbe3672b5bb488a
Jesus Ortega Bolviar <jesus.ortega.bolivar@gmail.com>
2021-11-11 15:37:28 +0100

    commit

---
+++ PrimerCore/PrimerCore/PrimerCore/Controllers/PruebaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerCore.Controllers
+{
+    public class PruebaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
